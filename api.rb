320422a2c8d08eca0019755910bd2c36398a3eaf
Jamie Ly <jamie.ly@gmail.com>
2012-09-29 06:42:00 -0400

    Added rack up file

--- api.rb
+++ api.rb
@@ -2,17 +2,21 @@ require 'sinatra'
 require 'json'
 require './word_lists'
 
-get '/api/v0.1/lists' do
-  content_type :json
-  WordLists.lists.keys.to_json
-end
+class BingoAPI < Sinatra::Base
+  get '/api/v0.1/lists' do
+    content_type :json
+    WordLists.lists.keys.to_json
+  end
 
-get '/api/v0.1/lists/:name' do
-  lists = WordLists.lists
+  get '/api/v0.1/lists/:name' do
+    lists = WordLists.lists
 
-  halt 404 unless lists.has_key? params[:name]
+    halt 404 unless lists.has_key? params[:name]
 
-  content_type :json
-  WordLists.lists[params[:name]].to_json
+    content_type :json
+    WordLists.lists[params[:name]].to_json
+  end
+
+  run! if app_file == $0
 end
 
